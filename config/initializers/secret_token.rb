ae3a0a8f1227b07c2f434b5ac921cc93f73c8ea9
Kevin Old <kevin@kevinold.com>
2016-02-03 21:14:34 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NashrbGraphqlDemo::Application.config.secret_key_base = 'c6c4637ce4727dd140b165cd3737ea7fd5e36dd1e4fe345c20869f19660771a66f9c65ddef24332bb6a57c3370caf376d60830e220c1b7d37b27bd1556ec1425'
