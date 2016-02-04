490b6b4ce2842947bed5d0c4ffa62d39f45ad5dd
Kevin Old <kevin@kevinold.com>
2016-02-03 23:16:26 -0600

    add author_id to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160204050433) do
+ActiveRecord::Schema.define(version: 20160204051414) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20160204050433) do
     t.text     "body"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "author_id"
   end
 
 end
